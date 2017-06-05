f7fd7662ca9852c2bbb63a5991af220f28abe7fb
Stephen J. Butler <stephen.butler@gmail.com>
2017-06-05 15:20:15 -0500

    Add script based provisioning

--- rds-wordpress.tf
+++ rds-wordpress.tf
@@ -86,4 +86,8 @@ resource "aws_db_instance" "wordpress" {
     lifecycle {
         # prevent_destroy = true
     }
+
+    provisioner "local-exec" {
+        command = "./scripts/provision-mysql.sh -h '${self.endpoint}' -u '${self.username}' -p '${self.password}' '${var.wp_db_name}' '${var.wp_db_user}' '${data.aws_kms_secret.secrets.wp_db_password}'"
+    }
 }
